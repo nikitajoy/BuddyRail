221f1d30e344f47e25105069a635543e5d0207e6
Nikita <nerdyaus21@gmail.com>
2024-08-04 19:50:56 +0300

    feat [frontend]: added application form component (in progress)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import TopNavbar from './components/TopNavbar.vue'
+import ApplicationForm from './components/ApplicationForm.vue'
+
 </script>
 
 <template>
+  <div>
 
+  <ApplicationForm :showing="applicationWindow"/>
   <TopNavbar />
+
+  </div>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/ApplicationForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-if="showing"> 
+        The modal will go here.
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showing: {
+      required: true,
+      type: Boolean
+    }
+  }
+}
+</script>
