97c77d4297342ff2b5343a7d0fcd10b07d9e1c03
Nikita <nerdyaus21@gmail.com>
2024-08-04 18:14:08 +0300

    frontend rebuild with vite and tailwindcss

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
 <template>
-<div>test</div>
+
+  <div>
+
+    <div>
+
+      <p class= "text-3xl font-bold underline">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </div>
+  </div>
 </template>
 
-<script>
-
-
-</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
-
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).mount('#app')
