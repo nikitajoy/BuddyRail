bfe7b080a420a6930b48f252b5747aa171e7f233
Nikita <nerdyaus21@gmail.com>
2024-08-24 07:34:06 +0300

    feat [frontend]: added 404 page

---
+++ frontend/src/pages/[...404].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        oops, not found
+        <router-link :to="`/`">Go to main page</router-link>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    created() {
+        this.$router.push('/')
+    }
+}
+</script>
